12d26e3c64606d7ce6411228adfe677a8b3623b2
luanxin <luanxin@social-touch.com>
2020-06-19 00:54:53 +0800

    @TODO how to init inner controller

--- src/main/java/com/lu/controller/ClusterMainController.java
+++ src/main/java/com/lu/controller/ClusterMainController.java
@@ -1,42 +1,16 @@
 package com.lu.controller;
 
-import com.lu.model.Topic;
-import com.lu.util.KafkaUtil;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import org.apache.kafka.clients.admin.AdminClient;
 
-import java.net.URL;
-import java.util.List;
-import java.util.ResourceBundle;
-import java.util.Set;
-import java.util.concurrent.ExecutionException;
-
-public class ClusterMainController extends RootController implements Initializable {
+public class ClusterMainController extends RootController {
     @FXML
     Label clusterNameLabel;
-    TopicController topicController;
-    List<String> bootstrapServerList;
-    AdminClient adminClient;
+    @FXML
+    private TopicController topicController;
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        String bootstrapServers = String.join(",", bootstrapServerList);
-        adminClient = KafkaUtil.getAdminClient(bootstrapServers);
-
-        try {
-            // TODO init table view
-            Set<String> topics = adminClient.listTopics().names().get();
-            List<Topic> topicList = KafkaUtil.getTopics(adminClient, topics);
-
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-
+    public ClusterMainController() {
+        topicController = new TopicController();
     }
 
     public Label getClusterNameLabel() {
@@ -53,21 +27,5 @@ public class ClusterMainController extends RootController implements Initializab
 
     public void setTopicController(TopicController topicController) {
         this.topicController = topicController;
-    }
-
-    public List<String> getBootstrapServerList() {
-        return bootstrapServerList;
-    }
-
-    public void setBootstrapServerList(List<String> bootstrapServerList) {
-        this.bootstrapServerList = bootstrapServerList;
-    }
-
-    public AdminClient getAdminClient() {
-        return adminClient;
-    }
-
-    public void setAdminClient(AdminClient adminClient) {
-        this.adminClient = adminClient;
     }
 }
--- src/main/java/com/lu/controller/TopicController.java
+++ src/main/java/com/lu/controller/TopicController.java
@@ -1,15 +1,21 @@
 package com.lu.controller;
 
 import com.lu.model.Topic;
+import com.lu.util.KafkaUtil;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.apache.kafka.clients.admin.AdminClient;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 public class TopicController extends RootController implements Initializable {
     @FXML
@@ -24,10 +30,48 @@ public class TopicController extends RootController implements Initializable {
     TableColumn countColumn;
     @FXML
     TableColumn consumerColumn;
-
-
+    List<String> bootstrapServerList;
+    AdminClient adminClient;
+    ObservableList<Topic> topicList;
+    // TODO 如何初始化内部controller的数据
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        ObservableList<Topic> list = FXCollections.observableArrayList();
+        topicList = FXCollections.observableArrayList();
+        String bootstrapServers = String.join(",", bootstrapServerList);
+        adminClient = KafkaUtil.getAdminClient(bootstrapServers);
+
+        try {
+            Set<String> topics = adminClient.listTopics().names().get();
+            List<Topic> topicList = KafkaUtil.getTopics(adminClient, topics);
+            buildTableView(topicList);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void buildTableView(List<Topic> topics) {
+        topicNameColumn.setCellValueFactory(new PropertyValueFactory<>("topicName"));
+        rfColumn.setCellValueFactory(new PropertyValueFactory<>("replicationFactor"));
+        partitionsColumn.setCellValueFactory(new PropertyValueFactory<>("partitions"));
+        countColumn.setCellValueFactory(new PropertyValueFactory<>("messageCount"));
+        consumerColumn.setCellValueFactory(new PropertyValueFactory<>("consumerCount"));
+        topicList.addAll(topics);
+        tableView.setItems(topicList);
+    }
+
+    public List<String> getBootstrapServerList() {
+        return bootstrapServerList;
+    }
+
+    public void setBootstrapServerList(List<String> bootstrapServerList) {
+        this.bootstrapServerList = bootstrapServerList;
+    }
+
+    public AdminClient getAdminClient() {
+        return adminClient;
+    }
+
+    public void setAdminClient(AdminClient adminClient) {
+        this.adminClient = adminClient;
     }
 }
