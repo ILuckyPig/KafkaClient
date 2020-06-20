4967485b532d595bb3ffdcac1d0bbfd0d2f33731
luanxin <luanxin@social-touch.com>
2020-06-20 23:45:39 +0800

    Topics&Consumers GUI change

--- src/main/java/com/lu/controller/ClusterMainController.java
+++ src/main/java/com/lu/controller/ClusterMainController.java
@@ -4,7 +4,6 @@ import com.lu.model.Cluster;
 import com.lu.util.KafkaUtil;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
@@ -18,7 +17,9 @@ public class ClusterMainController extends RootController {
     @FXML
     GridPane tableGridPane;
     private TopicsController topicsController;
+    private GridPane topicsGridPane;
     private ConsumersController consumersController;
+    private GridPane consumersGridPane;
     private Cluster cluster;
     private AdminClient adminClient;
 
@@ -36,36 +37,53 @@ public class ClusterMainController extends RootController {
      * @param mouseEvent
      */
     public void clickTopics(MouseEvent mouseEvent) {
-        if (null == topicsController) {
+        if (null == topicsGridPane) {
+            if (null != consumersGridPane) {
+                consumersGridPane.setVisible(false);
+            }
             try {
                 FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/TopicsFxml.fxml"));
-                Parent root = fxmlLoader.load();
+                topicsGridPane = fxmlLoader.load();
                 topicsController = fxmlLoader.getController();
                 topicsController.setAdminClient(adminClient);
                 topicsController.init();
-                tableGridPane.add(root,1,0);
+                tableGridPane.add(topicsGridPane,1,0);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         } else {
-
+            topicsGridPane.setVisible(true);
+            if (null != consumersGridPane) {
+                consumersGridPane.setVisible(false);
+            }
         }
     }
 
+    /**
+     * 显示Consumers界面
+     *
+     * @param mouseEvent
+     */
     public void clickConsumers(MouseEvent mouseEvent) {
-        if (null == consumersController) {
+        if (null == consumersGridPane) {
+            if (null != topicsGridPane) {
+                topicsGridPane.setVisible(false);
+            }
             try {
                 FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/ConsumersFxml.fxml"));
-                Parent root = fxmlLoader.load();
+                consumersGridPane = fxmlLoader.load();
                 consumersController = fxmlLoader.getController();
                 consumersController.setAdminClient(adminClient);
                 consumersController.init();
-                tableGridPane.add(root, 1, 0);
+                tableGridPane.add(consumersGridPane, 1, 0);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         } else {
-
+            consumersGridPane.setVisible(true);
+            if (null != topicsGridPane) {
+                topicsGridPane.setVisible(false);
+            }
         }
     }
 
