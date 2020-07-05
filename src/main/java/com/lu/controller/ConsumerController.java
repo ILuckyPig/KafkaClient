26f079b5ea538e27712a2e5cb1d53c5a118466d2
luanxin <luanxin@social-touch.com>
2020-07-05 23:25:55 +0800

    @TODO async consumer

--- src/main/java/com/lu/controller/ConsumerController.java
+++ src/main/java/com/lu/controller/ConsumerController.java
@@ -112,6 +112,7 @@ public class ConsumerController {
         recordListView.setItems(recordList);
     }
 
+    // TODO async consumer
     public void clickStart(MouseEvent mouseEvent) {
         String topic = topicChoiceBox.getSelectionModel().getSelectedItem();
         int partition = Integer.parseInt(partitionTextField.getText());
@@ -120,45 +121,40 @@ public class ConsumerController {
         ConsumerStartEnum startEnum = ConsumerStartEnum.from(startChoiceBox.getSelectionModel().getSelectedItem());
         ConsumerUntilEnum untilEnum = ConsumerUntilEnum.from(untilChoiceBox.getSelectionModel().getSelectedItem());
 
-        if (ConsumerStartEnum.OFFSET.equals(startEnum)) {
-            long offset = Long.parseLong(offsetTextField.getText());
-        }
-        if (ConsumerUntilEnum.NUMBER.equals(untilEnum)) {
-            int number = Integer.parseInt(numberTextField.getText());
-        }
-
         Properties properties = new Properties();
         properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, "kafka-client-" + System.currentTimeMillis());
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+
         if (ConsumerStartEnum.LATEST.equals(startEnum) && ConsumerUntilEnum.FOREVER.equals(untilEnum)) {
-
+            consumerMessage(startEnum, properties, topic, partition);
         } else if (ConsumerStartEnum.LATEST.equals(startEnum) && ConsumerUntilEnum.NUMBER.equals(untilEnum)) {
-
+            int number = Integer.parseInt(numberTextField.getText());
+            consumerMessage(startEnum, properties, topic, partition,  number);
         } else if (ConsumerStartEnum.EARLIEST.equals(startEnum) && ConsumerUntilEnum.FOREVER.equals(untilEnum)) {
-
+            consumerMessage(startEnum, properties, topic, partition);
         } else if (ConsumerStartEnum.EARLIEST.equals(startEnum) && ConsumerUntilEnum.NUMBER.equals(untilEnum)) {
-
+            int number = Integer.parseInt(numberTextField.getText());
+            consumerMessage(startEnum, properties, topic, partition, number);
         } else if (ConsumerStartEnum.OFFSET.equals(startEnum) && ConsumerUntilEnum.FOREVER.equals(untilEnum)) {
-
+            long offset = Long.parseLong(offsetTextField.getText());
+            consumerMessage(properties, topic, partition, offset);
         } else if (ConsumerStartEnum.OFFSET.equals(startEnum) && ConsumerUntilEnum.NUMBER.equals(untilEnum)) {
-
+            long offset = Long.parseLong(offsetTextField.getText());
+            int number = Integer.parseInt(numberTextField.getText());
+            consumerMessage(properties, topic, partition, offset, number);
         }
     }
-    // TODO consumer action mode
+
     public void consumerMessage(Properties properties, String topic, int partition, long offset, int number) {
         properties.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, number);
         KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
         TopicPartition topicPartition = new TopicPartition(topic, partition);
         kafkaConsumer.assign(Collections.singletonList(topicPartition));
         kafkaConsumer.seek(topicPartition, offset);
-        ConsumerRecords<String, String> consumerRecords = consumerRecords = kafkaConsumer.poll(Duration.ofSeconds(10));;
-        List<ConsumerRecord<String, String>> records = new ArrayList<>();
-        consumerRecords.iterator().forEachRemaining(records::add);
-        recordList.clear();
-        recordList.addAll(records);
+        refreshRecordList(kafkaConsumer);
     }
 
     public void consumerMessage(ConsumerStartEnum startEnum, Properties properties, String topic, int partition, int number) {
@@ -171,11 +167,7 @@ public class ConsumerController {
         } else {
             kafkaConsumer.seekToBeginning(Collections.singletonList(topicPartition));
         }
-        ConsumerRecords<String, String> consumerRecords = consumerRecords = kafkaConsumer.poll(Duration.ofSeconds(10));;
-        List<ConsumerRecord<String, String>> records = new ArrayList<>();
-        consumerRecords.iterator().forEachRemaining(records::add);
-        recordList.clear();
-        recordList.addAll(records);
+        refreshRecordList(kafkaConsumer);
     }
 
     public void consumerMessage(Properties properties, String topic, int partition, long offset) {
@@ -198,6 +190,14 @@ public class ConsumerController {
             kafkaConsumer.seekToBeginning(Collections.singletonList(topicPartition));
         }
         alwaysRefreshRecordList(kafkaConsumer);
+    }
+
+    public void refreshRecordList(KafkaConsumer<String, String> kafkaConsumer) {
+        ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofSeconds(10));;
+        List<ConsumerRecord<String, String>> records = new ArrayList<>();
+        consumerRecords.iterator().forEachRemaining(records::add);
+        recordList.clear();
+        recordList.addAll(records);
     }
 
     public void alwaysRefreshRecordList(KafkaConsumer<String, String> kafkaConsumer) {
--- src/test/java/ConsumerControllerTest.java
+++ src/test/java/ConsumerControllerTest.java
@@ -6,6 +6,5 @@ public class ConsumerControllerTest {
     public void testConsumerMessage() {
         ConsumerController controller = new ConsumerController();
         controller.setBootstrapServers("192.168.8.50:9092");
-        controller.consumerMessage(10,"xhs_user", 0, 200);
     }
 }
